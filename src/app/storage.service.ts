61083d8c28439fd411e687f83005116a1018f6ce
Daniele Rosada <danielerosada98@gmail.com>
2019-01-15 16:03:45 +0100

    resolve cors problem

--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -5,7 +5,7 @@ import { stringify } from '@angular/core/src/util';
   providedIn: 'root'
 })
 export class StorageService {
-  base_url = "ec2-34-251-72-250.eu-west-1.compute.amazonaws.com:3000";
+  base_url = "http://ec2-34-251-72-250.eu-west-1.compute.amazonaws.com:3000";
   options;
   constructor(private http: HttpClient) {
     this.getToken();
